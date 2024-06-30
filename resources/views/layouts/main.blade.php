c470e8a86eaae299948baa8185c5bd99b33407f3
France10 <victor.campos8@gmail.com>
2024-06-30 19:48:48 -0300

    uhu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -53,6 +53,8 @@
   <footer>
     <p>EliteEvents &copy; 2024</p>
   </footer>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
